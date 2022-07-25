5f302c073a45198f800d1914ddb2a58ae1ca4c4b
Максим Нестерев <jdb@MacBook-Air-Maksim.local>
2022-07-25 19:33:10 +0900

    AddTable

--- WeatherTwo/WeatherTwo/ViewController.swift
+++ WeatherTwo/WeatherTwo/ViewController.swift
@@ -2,7 +2,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Subviews
+    
+    let tableView: UITableView = {
+        let tableView = UITableView.init(
+            frame: .zero,
+            style: .plain
+        )
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
+
+    // MARK: - Life Cycle
+
+        setupView()
+        addSubviews()
+    }
+
+
+
+    // MARK: - Private
+
+    private func setupView() {
+        self.view.backgroundColor = UIColor.systemBlue
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
     }
 }
